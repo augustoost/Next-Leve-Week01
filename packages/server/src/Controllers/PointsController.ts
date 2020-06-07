8ac0ee72e6cd7b234eedff4bb54f68b0e581bb9b
Augusto Ostapechen <augusto_ostapechen@hotmail.com>
2020-06-07 17:49:22 -0300

    Added validation using celebrate and upload images with multer.

--- packages/server/src/Controllers/PointsController.ts
+++ packages/server/src/Controllers/PointsController.ts
@@ -17,7 +17,14 @@ class PoitnsController {
       .distinct()
       .select('points.*');
 
-    return response.json(points);
+      const serializedPoints = points.map(point => {
+        return {
+          ...point,
+          image_url: `http://192.168.0.102:3333/uploads/${point.image}`,
+        }
+      });
+
+    return response.json(serializedPoints);
   }
 
   async show (request: Request, response: Response) {
@@ -29,11 +36,16 @@ class PoitnsController {
       return response.status(400).json({message: 'Point not found'});
     }
 
+    const serializedPoint ={
+        ...point,
+        image_url: `http://192.168.0.102:3333/uploads/${point.image}`,
+    };
+
     const items = await knex('items')
       .join('points_items', 'items.id', '=', 'points_items .item_id')
       .where('points_items.point_id', id);
 
-    return response.json({point, items});
+    return response.json({point: serializedPoint, items});
   }
   
   async create(request: Request, response: Response){
@@ -47,11 +59,11 @@ class PoitnsController {
       uf,
       items
     } = request.body;
-  
+    
     const trx = await knex.transaction();
     
     const point = {
-      image: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+      image: request.file.filename,
       name,
       email,
       whatsapp,
@@ -65,15 +77,18 @@ class PoitnsController {
   
     const point_id = insertedIds[0];
   
-    const pointItems = items.map((item_id: number) => {
-      return {
-        item_id,
-        point_id,
-      }
+    const pointItems = items
+      .split(',')
+      .map((item: string) => Number(item.trim()))
+      .map((item_id: number) => {
+        return {
+          item_id,
+          point_id,
+        };
     });
   
     await trx('points_items').insert(pointItems);
-    
+      
     await trx.commit();
     
     return response.json({id: point_id, ...point,});
--- packages/server/src/routes.ts
+++ packages/server/src/routes.ts
@@ -1,18 +1,40 @@
-import express, { request, response } from 'express';
-import knex from './database/connection';
+import express from 'express';
+import { celebrate, Joi } from 'celebrate';
+
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import PointsController from './Controllers/PointsController';
 import ItemsController from './Controllers/ItemsController';
 
 const routes = express.Router();
+const upload = multer(multerConfig);
 
 const poitnsController = new PointsController();
 const itemsController = new ItemsController();
 
 routes.get('/items', itemsController.index);
-
-routes.post('/points', poitnsController.create);
 routes.get('/points', poitnsController.index);
 routes.get('/points/:id', poitnsController.show);
 
+routes.post(
+  '/points', 
+  upload.single('image'), 
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required(),
+      whatsapp: Joi.number().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      city: Joi.string().required(),
+      uf: Joi.string().required().max(2),
+      items: Joi.string().required(),
+    })
+  }, {
+    abortEarly: false
+  }),
+  poitnsController.create
+);
+ 
 export default routes;
